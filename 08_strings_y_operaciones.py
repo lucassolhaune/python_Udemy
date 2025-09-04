d80a4bba1374c59a679889aff90c7bb5406eeeab
Lucas Solhaune <lu.solhaune@gmail.com>
2025-09-03 21:44:52 -0300

    manejo de strings

---
+++ 08_strings_y_operaciones.py
@@ -0,0 +1,12 @@
+#Cadenas de texto y operaciones con strings
+
+palabra0 = "Fargo "
+print(palabra0) # Fargo
+print(palabra0[0]) # F
+
+palabra1 = "Lucas"
+print(palabra1)
+
+palabra2 = palabra0 + palabra1
+print(palabra2) # FargoLucas
+print(palabra2[0:4]) #Farg -> porque va desde la primer posicion hasta la cuarta
