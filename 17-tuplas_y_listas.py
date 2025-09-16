bc32d89ab3a487ace8af3383bee1aee48dca755b
Lucas Solhaune <lu.solhaune@gmail.com>
2025-09-16 11:31:34 -0300

    tuplas y listas

---
+++ 17-tuplas_y_listas.py
@@ -0,0 +1,32 @@
+#Tuplas inmutables
+
+colores = ("rojo", "verde", "azul")
+print(colores)
+
+#va a quedar siempre vacia porque no se puede modificar
+tupla_vacia = ()
+
+#puedo agregarle valores o modificarla
+lista_vacia = []
+
+#tupla de un solo elemento
+solo_una = (5,)
+print(solo_una)
+
+print(colores[0]) # rojo
+print(colores[-1]) # azul
+
+lista_colores = ["rojo", "verde", "azul"]
+lista_colores[0] = "negro"
+print(lista_colores) # ['negro', 'verde', 'azul']
+
+t1 = (1, 2, 3)
+t2 = (4, 5, 6)
+
+t3 = t1 + t2
+
+print(t3) #(1, 2, 3, 4, 5, 6)
+
+print(3 * t1)
+
+print(7 in t1) #False
